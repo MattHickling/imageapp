2250c662850299eb1f60d8fe5ec4f72b6085a578
Matt Hickling <m.hickling@hotmail.co.uk>
2023-07-10 20:06:20 +0100

    SEtting routes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,1 @@
+<h1>This is the __invoke method</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
+use Barryvdh\Debugbar\Facades\Debugbar;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    Debugbar::error('INFO');
-    return view('welcome');
-});
+// GET
+Route::get('/blog', [PostsController::class, 'index']);
+Route::get('/blog/{id}', [PostsController::class, 'show']);
+
+
+//POST
+Route::get('/blog/create', [PostsController::class, 'create']);
+Route::post('/blog/1', [PostsController::class, 'store']);
+
+// PUT or PATCH
+Route::get('/blog/edit/1', [PostsController::class, 'edit']);
+Route::post('/blog/1', [PostsController::class, 'update']);
+
+//DELETE
+Route::delete('/blog/1', [PostsController::class, 'destroy']);
+
+
+
+
+
+// Route::resource('/blog', PostsController::class);
+// Route::get('/', [HomeController::class, 'index']);
+Route::get('/', HomeController::class);
