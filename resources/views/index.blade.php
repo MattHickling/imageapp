6664c06ad69a03148c18543800257312da35c372
Matt Hickling <m.hickling@hotmail.co.uk>
2023-09-12 18:59:54 +0100

    index.blade home page added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <!-- Add Bootstrap CSS -->
+    <title>Welcome to My Blog</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
-<body>
-    <div class="container">
-        <!-- Example usage of Bootstrap classes -->
-        <h1 class="title">Tailwind 3.0</h1>
-        <button class="btn btn-primary">Click me</button>
+<body style="background-color: #f0f0f0;">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 text-center">
+                <h1 class="display-4 text-primary">Welcome to My Blog</h1>
+                <p class="lead">Explore the latest blog posts.</p>
+                <a href="{{ route('blog.index') }}" class="btn btn-primary btn-lg">Enter</a>
+            </div>
+        </div>
     </div>
 
-    <!-- Add Bootstrap JS -->
+    <!-- Bootstrap JS -->
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
